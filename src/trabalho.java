bdd72baf7342ba4a00a43ea2dc28e6912b026e8a
Kaueneto <kauecarddoso@gmail.com>
2024-05-29 18:58:37 -0300

    Initial commit

---
+++ src/trabalho.java
@@ -0,0 +1,318 @@
+import java.util.Scanner;
+
+public class trabalho {
+
+    public static void main ( String [] args){
+
+        boolean respostaCerta = false;
+          Scanner scanner = new Scanner(System.in);
+            String questao = " Quem quem foi o criador do java ? ";
+
+                do {System.out.println(questao);
+
+                    System.out.println("[1]Bill gates ");
+                    System.out.println("[2]james gosling");
+                    System.out.println("[3]elon musk");
+                    System.out.println("[4]jeff bezos");
+                    System.out.println("[5]steve jobs");
+
+                    System.out.println("digite a resposta");
+                    String resposta = scanner.nextLine();
+
+                    if (resposta.equals("2")){
+                  System.out.println("resposta correta");
+                  respostaCerta = true;
+                }   else {
+                        System.out.println("resposta errada");
+                    }  }while (true!=respostaCerta);
+
+
+
+
+        String questao2 = " Quem quem foi o criador da micrsoft ? ";
+
+        do {System.out.println(questao2);
+
+            System.out.println("[1]Bill gates ");
+            System.out.println("[2]james gosling");
+            System.out.println("[3]elon musk");
+            System.out.println("[4]jeff bezos");
+            System.out.println("[5]steve jobs");
+
+            System.out.println("digite a resposta");
+            String resposta = scanner.nextLine();
+
+            if (resposta.equals("5")){
+                System.out.println("resposta correta");
+                respostaCerta = true;
+            }   else {
+                System.out.println("resposta errada");
+            }  }while (true!=respostaCerta);
+
+        String questao3 = " Quem quem foi o criador de minecraft ";
+
+        do {System.out.println(questao3);
+
+            System.out.println("[1]Bill gates ");
+            System.out.println("[2]minecraft");
+            System.out.println("[3]marcus persson");
+            System.out.println("[4]eloy");
+            System.out.println("[5]bob esponja");
+
+            System.out.println("digite a resposta");
+            String resposta = scanner.nextLine();
+
+            if (resposta.equals("3")){
+                System.out.println("resposta correta");
+                respostaCerta = true;
+            }   else {
+                System.out.println("resposta errada");
+            }  }while (true!=respostaCerta);
+
+        String questao4 = " para que serve a madeira no minecraft ? ";
+
+        do {System.out.println(questao4);
+
+            System.out.println("[1]pra tudo ");
+            System.out.println("[2]para craft");
+            System.out.println("[3]para construir");
+            System.out.println("[4]para observa");
+            System.out.println("[5]pra nada");
+
+            System.out.println("digite a resposta");
+            String resposta = scanner.nextLine();
+
+            if (resposta.equals("1")){
+                System.out.println("resposta correta");
+                respostaCerta = true;
+            }   else {
+                System.out.println("resposta errada");
+            }  }while (true!=respostaCerta);
+
+        String questao5 = " o machado serve pra que ? ";
+
+        do {System.out.println(questao5);
+
+            System.out.println("[1]enfeite ");
+            System.out.println("[2]para nada");
+            System.out.println("[3]quebrar tudo de madeira");
+            System.out.println("[4]minerar");
+            System.out.println("[5]girar");
+
+            System.out.println("digite a resposta");
+            String resposta = scanner.nextLine();
+
+            if (resposta.equals("3")){
+                System.out.println("resposta correta");
+                respostaCerta = true;
+            }   else {
+                System.out.println("resposta errada");
+            }  }while (true!=respostaCerta);
+
+        String questao6 = " qual animal mais movel do minecraft ? ";
+
+        do {System.out.println(questao6);
+
+            System.out.println("[1] peixe ");
+            System.out.println("[2] cavalo ");
+            System.out.println("[3] boi");
+            System.out.println("[4] feijao");
+            System.out.println("[5] galinha");
+
+            System.out.println("digite a resposta");
+            String resposta = scanner.nextLine();
+
+            if (resposta.equals("2")){
+                System.out.println("resposta correta");
+                respostaCerta = true;
+            }   else {
+                System.out.println("resposta errada");
+            }  }while (true!=respostaCerta);
+
+        String questao7 = " quais inimigos podem explodir ? ";
+
+        do {System.out.println(questao7);
+
+            System.out.println("[1]gato");
+            System.out.println("[2]cama");
+            System.out.println("[3]peixe");
+            System.out.println("[4] stve");
+            System.out.println("[5] creeper");
+
+            System.out.println("digite a resposta");
+            String resposta = scanner.nextLine();
+
+            if (resposta.equals("5")){
+                System.out.println("resposta correta");
+                respostaCerta = true;
+            }   else {
+                System.out.println("resposta errada");
+            }  }while (true!=respostaCerta);
+
+
+        String questao8 = " o enderman toma banho? ";
+
+        do {System.out.println(questao8);
+
+            System.out.println("[1] so na chuva ");
+            System.out.println("[2] sim ");
+            System.out.println("[3] quando pesca ");
+            System.out.println("[4] nao prescisa");
+            System.out.println("[5]sim para lavar o pe");
+
+            System.out.println("digite a resposta");
+            String resposta = scanner.nextLine();
+
+            if (resposta.equals("4")){
+                System.out.println("resposta correta");
+                respostaCerta = true;
+            }   else {
+                System.out.println("resposta errada");
+            }  }while (true!=respostaCerta);
+
+
+        String questao9 = " minecraft ainda é jogavel ? ";
+
+        do {System.out.println(questao9);
+
+            System.out.println("[1] uma vez ao ano ");
+            System.out.println("[2] Digite F ");
+            System.out.println("[3]sim ate hoje ");
+            System.out.println("[4] nao fecharam");
+            System.out.println("[5]so com mods");
+
+            System.out.println("digite a resposta");
+            String resposta = scanner.nextLine();
+
+            if (resposta.equals("3")){
+                System.out.println("resposta correta");
+                respostaCerta = true;
+            }   else {
+                System.out.println("resposta errada");
+            }  }while (true!=respostaCerta);
+
+
+        String questao10 = " o herobrine e verdadeiro? ";
+
+        do {System.out.println(questao10);
+
+            System.out.println("[1] nao apenas mito ");
+            System.out.println("[2]sim claro");
+            System.out.println("[3]nao virou uber ");
+            System.out.println("[4]sim e o stve");
+            System.out.println("[5]ele esta no the end");
+
+            System.out.println("digite a resposta");
+            String resposta = scanner.nextLine();
+
+            if (resposta.equals("1")){
+                System.out.println("resposta correta");
+                respostaCerta = true;
+            }   else {
+                System.out.println("resposta errada");
+            }  }while (true!=respostaCerta);
+
+        String questao11 = " qual camada tem diamente? ";
+
+        do {System.out.println(questao11);
+
+            System.out.println("[1]11 ");
+            System.out.println("[2]12");
+            System.out.println("[3]13");
+            System.out.println("[4]14");
+            System.out.println("[5]15");
+
+            System.out.println("digite a resposta");
+            String resposta = scanner.nextLine();
+
+            if (resposta.equals("2")){
+                System.out.println("resposta correta");
+                respostaCerta = true;
+            }   else {
+                System.out.println("resposta errada");
+            }  }while (true!=respostaCerta);
+
+        String questao12 = " espada de diamante ea mais fortes ? ";
+
+        do {System.out.println(questao12);
+
+            System.out.println("[1]sim ");
+            System.out.println("[2]nao a de  batata");
+            System.out.println("[3]nao a de couro");
+            System.out.println("[4]nao a de netherit");
+            System.out.println("[5]nao");
+
+            System.out.println("digite a resposta");
+            String resposta = scanner.nextLine();
+
+            if (resposta.equals("4")){
+                System.out.println("resposta correta");
+                respostaCerta = true;
+            }   else {
+                System.out.println("resposta errada");
+            }  }while (true!=respostaCerta);
+
+        String questao13 = " faz sentidos jogar sem mods ? ";
+
+        do {System.out.println(questao13);
+
+            System.out.println("[1]em emulador");
+            System.out.println("[2]sim com a dlc");
+            System.out.println("[3]claro é melhor");
+            System.out.println("[4]depende da onde mais nao ");
+            System.out.println("[5]fica sem graça");
+
+            System.out.println("digite a resposta");
+            String resposta = scanner.nextLine();
+
+            if (resposta.equals("4")){
+                System.out.println("resposta correta");
+                respostaCerta = true;
+            }   else {
+                System.out.println("resposta errada");
+            }  }while (true!=respostaCerta);
+
+
+        String questao14 = " enfretar o dragao  e o fim ? ";
+
+        do {System.out.println(questao14);
+
+            System.out.println("[1] nao ");
+            System.out.println("[2] sim perde a graça");
+            System.out.println("[3] sim na dlc ");
+            System.out.println("[4] sem mod sim ");
+            System.out.println("[5] sim");
+
+            System.out.println("digite a resposta");
+            String resposta = scanner.nextLine();
+
+            if (resposta.equals("1")){
+                System.out.println("resposta correta");
+                respostaCerta = true;
+            }   else {
+                System.out.println("resposta errada");
+            }  }while (true!=respostaCerta);
+
+
+
+                String questao15 = " qual maça é melhor ? ";
+
+                do {System.out.println(questao15);
+
+                    System.out.println("[1]encantada");
+                    System.out.println("[2]normal");
+                    System.out.println("[3]de ouro");
+                    System.out.println("[4]de mod");
+                    System.out.println("[5]de codigo");
+
+                    System.out.println("digite a resposta");
+                    String resposta = scanner.nextLine();
+
+                    if (resposta.equals("1")){
+                        System.out.println("resposta correta");
+                        respostaCerta = true;
+
+                    }   else{ System.out.println("resposta errada");
+                    }  }while (true!=respostaCerta);
+
+} }
